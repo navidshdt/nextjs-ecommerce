fba65509ed3959156b83d735f5fae139959a641c
Navid Shirandasht <navidshirandasht@gmail.com>
2026-09-22 22:34:13 +0330

    change currency format

--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -10,14 +10,14 @@ function PriceInput({ defaultValue }: FormInputNumberProps) {
   return (
     <div className="mb-2">
       <Label htmlFor="price" className="capitalize">
-        Price ($)
+        Price (Toman)
       </Label>
       <Input
         id={name}
         type="number"
         name={name}
         min={0}
-        defaultValue={defaultValue || 100}
+        defaultValue={defaultValue || 100000}
         required
       />
     </div>
